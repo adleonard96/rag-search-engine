c267cfdc96efa2640b2ff24b5323f68f7e8c2bc8
adleonard96 <alexanderleonard27@gmail.com>
2026-01-19 14:28:23 -0500

    finished RAG course

--- cli/test_gemini.py
+++ cli/test_gemini.py
@@ -4,7 +4,6 @@ from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-print(f"Using key {api_key[:6]}...")
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
